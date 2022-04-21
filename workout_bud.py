527b2e99d16a73e47cd7515778714cd56382bbc6
ETingRice <ethanting888@gmail.com>
2022-04-21 13:36:50 -0500

    Working pushups

--- workout_bud.py
+++ workout_bud.py
@@ -4,16 +4,35 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-def push_up(im_list):
+#count = 0
+position = None
 
-    return
+def push_up(imlist):
+    global position
+    if(imlist[12][2] >= imlist[14][2] and imlist[11][2] >= imlist[13][2]):
+        position="down"
+    if(imlist[12][2] <= imlist[14][2] and imlist[11][2] <= imlist[13][2]) and position=="down":
+        position = "up"
+        return True
+    return False 
 
+
+'''
+def squats(imlist):
+    global position
+    if(imlist[24][2] <= imlist[26][2] and imlist[23][2] <= imlist[25][2]):
+        position ="down"
+    if(imlist[24][2] >= imlist[26][2] and imlist[23][2] >= imlist[25][2]) and position=="down":
+        position = "up"
+        return True
+    return False
+'''
 
 # For webcam input:
 def workout():
     count = 0
-    position = None
-    
+    #position = None
+    poses = ''
     cap = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX
     with mp_pose.Pose(
@@ -48,20 +67,26 @@ def workout():
                     h,w,_=image.shape
                     X,Y=int(im.x*w),int(im.y*h)
                     imlist.append([id,X,Y])
-            
+
+                past_pose = pose
+                if(past_pose != pose):
+                    count = 0
+                    position=None
+
                 if len(imlist) != 0:
-                    if(imlist[12][2] >= imlist[14][2] and imlist[11][2] >= imlist[13][2]):
-                        position="down"
-                    if(imlist[12][2] <= imlist[14][2] and imlist[11][2] <= imlist[13][2]) and position=="down":
-                        position = "up"
+                    if push_up(imlist):
+                        poses = 'Push-ups: '
                         count+=1
                         print(count)
-
+                   ''' if squats(imlist):
+                        poses = 'Squats: '
+                        count+=1
+                        print(count)'''
 
             # Flip the image horizontally for a selfie-view display.
             image=cv2.flip(image,1)
             cv2.putText(image, 
-                    'Push-ups: ' + str(count), 
+                    str(poses) + str(count), 
                     (50, 50), 
                     font, 1, 
                     (0, 0, 0), 
